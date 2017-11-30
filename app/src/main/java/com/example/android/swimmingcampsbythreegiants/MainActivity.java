d26f49e81a17024eab726a4c178f03440aea6b76
Zsolt <zsolt.roesler@gmail.com>
2017-11-30 19:59:18 +0100

    Update on MainActivity.java

--- app/src/main/java/com/example/android/swimmingcampsbythreegiants/MainActivity.java
+++ app/src/main/java/com/example/android/swimmingcampsbythreegiants/MainActivity.java
@@ -6,46 +6,70 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    WebView text2;
+
+    private WebView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text2 = (WebView) findViewById(R.id.text2);
-                 String text = "<html><body>"
-                  + "<p align=\"justify\">"
-                         + getString(R.string.text2)
-                           + "</p> "
-                        + "</body></html>";
-                     text2.loadData(text, "text/html", "utf-8");
+        // This method called to justify the description text
+        description = (WebView) findViewById(R.id.webview_description);
+        String text = "<html><body>"
+                + "<p align=\"justify\">"
+                + getString(R.string.text)
+                + "</p> "
+                + "</body></html>";
+        description.loadData(text, "text/html", "utf-8");
 
-          }
 
-    public void call(View view) {
-        Intent intent = new Intent(Intent.ACTION_DIAL);
-        intent.setData(Uri.parse("tel:+436604975475"));
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
+        // Click on the call line to initiate a call
+        LinearLayout call = (LinearLayout) findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to initiate a call
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + getString(R.string.tellNumber)));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
 
-    public void email(View view) {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:"));
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"office@three-giants.com"});
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
-    public void web(View view) {
-        Uri webpage = Uri.parse("http://www.three-giants.com/schwimmkurse/threegiants-schwimmcamp-sudstadt");
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
+        // Click on the email line to compose an email
+        LinearLayout email = (LinearLayout) findViewById(R.id.email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to call implicit intent email app
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                // "mailto:" only email apps can handle it
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{getString(R.string.emailAddress)});
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.emailSubject));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        // Click on the web line to open a browser
+        LinearLayout web = (LinearLayout) findViewById(R.id.web);
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to call implicit intent web browser app
+                Uri webpage = Uri.parse(getString(R.string.url));
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
